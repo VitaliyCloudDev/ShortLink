52fe87fac1f95b472008058029ace3ea35f9949c
VitaliyCloudDev <vitaliylan1@gmail.com>
2025-07-16 09:20:29 +0300

    added GET request process

--- main.go
+++ main.go
@@ -1,20 +1,30 @@
 package main
 
 import (
-	"flag"
-	"fmt"
 	"net/http"
+
+	"github.com/gin-gonic/gin"
 )
 
+type link struct {
+	ID       string `json:"id"`
+	UrlOrig  string `json:"url"`
+	ShortUrl string `json:"ShortUrl"`
+	Time     string `json:"Time"`
+}
+
+var links = []link{
+	{ID: "0",
+		UrlOrig:  "https://testlink.com/database/index012345",
+		ShortUrl: "https://shortlink.com/0"},
+}
+
+func getUrlData(c *gin.Context) {
+	c.IndentedJSON(http.StatusOK, links)
+}
+
 func main() {
-	http.HandleFunc("/", HelloServer)
-	http.ListenAndServe(":8080", nil)
-}
-
-func HelloServer(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(w, "Redirecting...")
-}
-
-func getFlags() {
-	flag.Parse()
+	router := gin.Default()
+	router.GET("/links", getUrlData)
+	router.Run("localhost:8080")
 }
